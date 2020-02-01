e2c126650a6ca651e101866b5a2107f08fb1a49b
Haidar Zamzam <haidar.zamzam99@gmail.com>
2020-02-02 00:59:26 +0700

    Add Logic and fixing UI

--- SimpleLoginApps/ViewController.swift
+++ SimpleLoginApps/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPin: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func showAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                         
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                         
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+            
 
+    @IBAction func clickLogin(_ sender: UIButton) {
+        let email:String = tfEmail.text!
+        let pin:String = tfPin.text!
+               
+        if (email.isEmpty || pin.isEmpty){
+            showAlert(title: "Warning", message: "Isi Email/Password")
+        }else if(email == "admin@admin.com" && pin == "12345"){
+            showAlert(title: "Success", message: "Berhasil Login :)")
+        }else{
+            showAlert(title: "Failed", message: "Email/Pin Salah!")
+        }
+    }
 }
 
