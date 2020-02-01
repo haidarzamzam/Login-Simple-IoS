936735629b6692e746422bc1b33e3816ea0c1143
Haidar Zamzam <haidar.zamzam99@gmail.com>
2020-02-02 00:41:38 +0700

    Initial Commit

---
+++ SimpleLoginApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleLoginApps
+//
+//  Created by Haidar Zamzam on 02/02/20.
+//  Copyright © 2020 Haidar Zamzam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
